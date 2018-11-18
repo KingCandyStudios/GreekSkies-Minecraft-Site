86809c2d31192143ddb8af269d621ce79db143c3
KingCandyStudios <45146712+KingCandyStudios@users.noreply.github.com>
2018-11-18 07:28:19 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b5d35e7df040c3e9e0c0e9a/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+  <meta charset="UTF-8">
+  <title>GreekSkies - Loading</title>
+<meta http-equiv="refresh" content="5;url=http://greekskies.ezyro.com/home/" />
+
+  <link href='https://fonts.googleapis.com/css?family=Anton:100,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  <div class="start-screen">
+
+  <div class="loading">
+    <div class="loading__element el1">W</div>
+    <div class="loading__element el2">E</div>
+    <div class="loading__element el3">B</div>
+    <div class="loading__element el4">S</div>
+    <div class="loading__element el5">I</div>
+    <div class="loading__element el6">T</div>
+    <div class="loading__element el7">E</div>
+  </div>
+  
+    <div class="loading">
+    <div class="loading__element el1">L</div>
+    <div class="loading__element el2">O</div>
+    <div class="loading__element el3">A</div>
+    <div class="loading__element el4">D</div>
+    <div class="loading__element el5">I</div>
+    <div class="loading__element el6">N</div>
+    <div class="loading__element el7">G</div>
+  </div>
+  
+</div>
+  
+  
+
+</body>
+
+</html>
